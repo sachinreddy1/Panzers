f94b29e133dbce761501d4b26be00079dbd09e19
sachinreddy1 <sachinreddy1009@gmail.com>
2019-08-18 15:41:08 -0500

    movement updates.

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -4,34 +4,78 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
-    public float movementSpeed = 4f;
-    public float horizontalMove = 0f;
-    public float verticalMove = 0f;
+    public int m_PlayerNumber = 1;
+    public float m_Speed = 12f;
+    public float m_TurnSpeed = 180f;
 
-    private Vector3 m_Velocity = Vector3.zero;
-    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;
-    private Rigidbody m_Rigidbody;
+    private string m_MovementAxisName;          // The name of the input axis for moving forward and back.
+    private string m_TurnAxisName;              // The name of the input axis for turning.
+    private Rigidbody m_Rigidbody;              // Reference used to move the tank.
+    private float m_MovementInputValue;         // The current value of the movement input.
+    private float m_TurnInputValue;             // The current value of the turn input.
 
-    private void Awake()
-	{
-		m_Rigidbody = GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
-    void Update()
+    private void Awake ()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * movementSpeed;
-        verticalMove = Input.GetAxisRaw("Vertical") * movementSpeed;
+        m_Rigidbody = GetComponent<Rigidbody> ();
     }
 
-    void FixedUpdate() {            
-        Move();
+    private void OnEnable ()
+    {
+        // When the tank is turned on, make sure it's not kinematic.
+        m_Rigidbody.isKinematic = false;
+
+        // Also reset the input values.
+        m_MovementInputValue = 0f;
+        m_TurnInputValue = 0f;
     }
 
-    void Move() {
-        Vector3 targetVelocity = new Vector3(horizontalMove * 10f, 0f, verticalMove * 10f);
-        m_Rigidbody.velocity = Vector3.SmoothDamp(m_Rigidbody.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+    private void OnDisable ()
+    {
+        // When the tank is turned off, set it to kinematic so it stops moving.
+        m_Rigidbody.isKinematic = true;
     }
 
+    private void Start ()
+    {
+        // The axes names are based on player number.
+        m_MovementAxisName = "Vertical";
+        m_TurnAxisName = "Horizontal";
+    }
+
+    private void Update ()
+    {
+        // Store the value of both input axes.
+        m_MovementInputValue = Input.GetAxis (m_MovementAxisName);
+        m_TurnInputValue = Input.GetAxis (m_TurnAxisName);
+    }
+
+    private void FixedUpdate ()
+    {
+        // Adjust the rigidbodies position and orientation in FixedUpdate.
+        Move ();
+        Turn ();
+    }
+
+    private void Move ()
+    {
+        // Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames.
+        Vector3 movement = -transform.right * m_MovementInputValue * m_Speed * Time.deltaTime;
+
+        // Apply this movement to the rigidbody's position.
+        m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+    }
+
+
+    private void Turn ()
+    {
+        // Determine the number of degrees to be turned based on the input, speed and time between frames.
+        float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+
+        // Make this into a rotation in the y axis.
+        Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
+
+        // Apply this rotation to the rigidbody's rotation.
+        m_Rigidbody.MoveRotation (m_Rigidbody.rotation * turnRotation);
+    }
 
 }
