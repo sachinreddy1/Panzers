ff275adb688c5e96c6851a512049c732b769f8c7
sachinreddy1 <sachinreddy1009@gmail.com>
2019-08-18 19:29:53 -0500

    cameraContoller added.

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -8,11 +8,11 @@ public class playerController : MonoBehaviour
     public float m_Speed = 12f;
     public float m_TurnSpeed = 180f;
 
-    private string m_MovementAxisName;          // The name of the input axis for moving forward and back.
-    private string m_TurnAxisName;              // The name of the input axis for turning.
-    private Rigidbody m_Rigidbody;              // Reference used to move the tank.
-    private float m_MovementInputValue;         // The current value of the movement input.
-    private float m_TurnInputValue;             // The current value of the turn input.
+    private string m_MovementAxisName;
+    private string m_TurnAxisName;
+    private Rigidbody m_Rigidbody;
+    private float m_MovementInputValue;
+    private float m_TurnInputValue;
 
     private void Awake ()
     {
@@ -21,60 +21,46 @@ public class playerController : MonoBehaviour
 
     private void OnEnable ()
     {
-        // When the tank is turned on, make sure it's not kinematic.
         m_Rigidbody.isKinematic = false;
 
-        // Also reset the input values.
         m_MovementInputValue = 0f;
         m_TurnInputValue = 0f;
     }
 
     private void OnDisable ()
     {
-        // When the tank is turned off, set it to kinematic so it stops moving.
         m_Rigidbody.isKinematic = true;
     }
 
     private void Start ()
     {
-        // The axes names are based on player number.
         m_MovementAxisName = "Vertical";
         m_TurnAxisName = "Horizontal";
     }
 
     private void Update ()
     {
-        // Store the value of both input axes.
         m_MovementInputValue = Input.GetAxis (m_MovementAxisName);
         m_TurnInputValue = Input.GetAxis (m_TurnAxisName);
     }
 
     private void FixedUpdate ()
     {
-        // Adjust the rigidbodies position and orientation in FixedUpdate.
         Move ();
         Turn ();
     }
 
     private void Move ()
     {
-        // Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames.
         Vector3 movement = -transform.right * m_MovementInputValue * m_Speed * Time.deltaTime;
-
-        // Apply this movement to the rigidbody's position.
         m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
     }
 
 
     private void Turn ()
     {
-        // Determine the number of degrees to be turned based on the input, speed and time between frames.
         float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
-
-        // Make this into a rotation in the y axis.
         Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
-
-        // Apply this rotation to the rigidbody's rotation.
         m_Rigidbody.MoveRotation (m_Rigidbody.rotation * turnRotation);
     }
 
